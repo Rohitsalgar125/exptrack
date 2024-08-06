7d92d09535b6326138dfd7a903ad55f650473c1b
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-06 17:16:26 +0530

    Update budgetValidation.js

--- validation/expenseValidation/budgetValidation.js
+++ validation/expenseValidation/budgetValidation.js
@@ -1,20 +1,5 @@
 const { check, validationResult } = require("express-validator");
 
-const addBudgetValidation = [
-    check("userId").notEmpty().withMessage("userId can not be empty"),
-    check("amount").notEmpty().withMessage("budget can not be empty"),
-      check("description").notEmpty().withMessage("description can not be empty"),
-    
-
-    (req, res, next) => {
-        let errors = validationResult(req).array();
-        if (errors.length > 0) {
-            return res.send({ status: 0, message: errors[0].msg });
-        }
-        return next();
-    },
-];
-
 
 const deleteExpenseValidation = [
     check("budgetId").notEmpty().withMessage("Budget Id can not be empty"),
@@ -30,8 +15,9 @@ const deleteExpenseValidation = [
 ];
 
 const addExpenseValidation = [
-    check("spending").notEmpty().withMessage("Spending can not be empty"),
-    check("category").notEmpty().withMessage("Category can not be empty"),
+       check("userId").notEmpty().withMessage("userId can not be empty"),
+    check("amount").notEmpty().withMessage("budget can not be empty"),
+      check("description").notEmpty().withMessage("description can not be empty"),
 
     (req, res, next) => {
         let errors = validationResult(req).array();
@@ -43,8 +29,6 @@ const addExpenseValidation = [
 ];
 
 module.exports = {
-    addBudgetValidation,
-    addExpenseValidation,
     deleteExpenseValidation,
     addExpenseValidation
 };
