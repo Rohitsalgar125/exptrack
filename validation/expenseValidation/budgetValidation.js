46b80aa27602cf875d0350c07523635a591b7b27
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-06 17:19:22 +0530

    Update budgetValidation.js

--- validation/expenseValidation/budgetValidation.js
+++ validation/expenseValidation/budgetValidation.js
@@ -16,7 +16,7 @@ const deleteExpenseValidation = [
 
 const addExpenseValidation = [
        check("userId").notEmpty().withMessage("userId can not be empty"),
-    check("amount").notEmpty().withMessage("budget can not be empty"),
+    check("amount").notEmpty().withMessage("amount can not be empty"),
       check("description").notEmpty().withMessage("description can not be empty"),
 
     (req, res, next) => {
