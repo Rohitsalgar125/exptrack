4ab903d21d9633feaa36844487fa1a08b103d5fe
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-06 17:12:03 +0530

    Update budgetRoute.js

--- route/budgetRoute.js
+++ route/budgetRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { addBudget, addExpense, deleteExpense, deleteBudget } = require("../controller/budgetController");
+const {  addExpense, deleteExpense } = require("../controller/budgetController");
 const { addBudgetValidation, deleteExpenseValidation, addExpenseValidation } = require("../validation/expenseValidation/budgetValidation");
 // const { authChecker } = require("../common/common");
 let budgetRouter = express.Router();
 
-budgetRouter.post("/addBudget",addBudgetValidation, addBudget);
-budgetRouter.post("/deleteBudget", deleteBudget);
+budgetRouter.post("/addExpense",addBudgetValidation, addExpense);
+budgetRouter.post("/deleteExpense", deleteExpense);
 
 module.exports = budgetRouter;
