570f6328af227653f2adf9381ce2d95837c54687
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-06 17:11:07 +0530

    Update budgetController.js

--- controller/budgetController.js
+++ controller/budgetController.js
@@ -4,13 +4,13 @@ const budget = require('../schema/expense/budget');
 const objectId = mongoose.Types.ObjectId;
 
 
-const addBudget = async (req, res) => {
+const addExpense = async (req, res) => {
     let payload;
     try {
         payload = req.body;
         if (Object.keys(payload).length > 0) {
             await budget.create(payload);
-            res.send({ status: 1, repsponse: "Budget Added Successfully" });
+            res.send({ status: 1, repsponse: "Expense Added Successfully" });
         }
         else {
             res.send({ status: 0, repsponse: "Data Not Found" });
@@ -20,17 +20,17 @@ const addBudget = async (req, res) => {
     }
 }
 
-const deleteBudget = async (req, res) => {
+const deleteExpense = async (req, res) => {
     let payload, deleteReturn;
     try {
         payload = req.body;
         if (Object.keys(payload).length > 0) {
             deleteReturn = await budget.findByIdAndDelete({ _id: new objectId(payload) })
             if (deleteReturn !== null) {
-                return res.send({ status: 1, repsponse: "Budget Deleted Successfully" })
+                return res.send({ status: 1, repsponse: "Expense Deleted Successfully" })
             }
             else {
-                return res.send({ status: 0, repsponse: "Budget ID can not found" })
+                return res.send({ status: 0, repsponse: "Expense ID can not found" })
             }
         }
         else {
@@ -44,6 +44,6 @@ const deleteBudget = async (req, res) => {
 
 
 module.exports = {
-    addBudget,
-    deleteBudget,
+    addExpense,
+    deleteExpense,
 }
