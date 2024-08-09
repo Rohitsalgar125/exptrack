c1224c4c178eaf466bc823908e275a483dd67649
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-09 11:31:00 +0530

    Update groupController.js

--- controller/groupController.js
+++ controller/groupController.js
@@ -1,1 +1,32 @@
 
+
+const { default: mongoose } = require('mongoose');
+const group = require('../schema/group');
+const objectId = mongoose.Types.ObjectId;
+
+
+const addGroup = async (req, res) => {
+    let payload;
+    try {
+        payload = req.body;
+        if (Object.keys(payload).length > 0) {
+            await group.create(payload);
+            res.send({ status: 1, repsponse: "Group Added Successfully" });
+        }
+        else {
+            res.send({ status: 0, repsponse: "Data Not Found" });
+        }
+    } catch (error) {
+        res.send({ status: 0, repsponse: error.message });
+    }
+}
+
+
+
+     
+
+
+
+module.exports = {
+    addGroup,
+}
