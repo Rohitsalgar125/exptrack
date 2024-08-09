3f65037110527ee1c86f3a077248e846b95177fe
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-09 11:41:57 +0530

    Update groupRoute.js

--- route/groupRoute.js
+++ route/groupRoute.js
@@ -1,8 +1,6 @@
 
 const express = require("express");
 const {  addGroup } = require("../controller/groupController");
-const {  addExpenseValidation } = require("../validation/expenseValidation/budgetValidation");
-// const { authChecker } = require("../common/common");
 let groupRouter = express.Router();
 
 budgetRouter.post("/addGroup",addGroup );
