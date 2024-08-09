89140d2c4d146ba1c0b5a1f3622f02c9cb100300
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-09 11:57:01 +0530

    Update groupValidation.js

--- validation/groupValidation.js
+++ validation/groupValidation.js
@@ -1,1 +1,21 @@
+const { check, validationResult } = require("express-validator");
+
+
+
+const addGroupValidation = [
+    check("groupName").notEmpty().withMessage("groupName can not be empty"),
+    check("groupType").notEmpty().withMessage("groupType can not be empty"),
+
+    (req, res, next) => {
+        let errors = validationResult(req).array();
+        if (errors.length > 0) {
+            return res.send({ status: 0, message: errors[0].msg });
+        }
+        return next();
+    },
+];
+
+module.exports = {
+    addGroupValidation
+};
 
