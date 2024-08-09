8ee8fd604bdfd9bae67c3953f16dc1e7cd8c6671
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2024-08-09 11:27:57 +0530

    Update groupSchema.js

--- schema/groupSchema.js
+++ schema/groupSchema.js
@@ -1,1 +1,14 @@
+const mongoose = require("mongoose");
 
+
+const groupSchema = mongoose.Schema({
+  groupName: { type: String, required: true },
+  groupType: { type: String, enum: ['Home', 'Trip'], required: true },
+  groupPhoto: { type: String },
+},{
+    versionKey : false ,
+    timestamps : true
+})
+
+
+module.exports = mongoose.model('group',groupSchema);
